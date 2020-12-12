96eb7f9b2e6fc620d6487d898a8fd5b9839ebe41
GRasul99 <rasulganiyev1999@gmail.com>
2020-12-12 23:41:13 +0500

    Installed Tailwind css

---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  name: 'TheNavBar'
+}
+</script>
+
+<style scoped></style>
--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="home"></div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Register'
 }
 </script>
+
+<style scoped></style>
